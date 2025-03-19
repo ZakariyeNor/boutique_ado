346685cb7c004de64c302a7c291bb84a74c879f6
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-03-19 08:07:49 +0000

    Add sorting and directions functionality

--- products/views.py
+++ products/views.py
@@ -11,8 +11,22 @@ def all_products(request):
     products = Products.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
