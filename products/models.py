b4880074c173473d1f035c12ca51b87f54ef06c2
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-05 09:05:54 +0000

    display products

--- products/models.py
+++ products/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
-    class Meta:
-        verbose_name_plural = 'Categories'
-
     
     def get_freindly_name(self):
         return self.friendly_name
