ac86fb422dd05993cb5fadd89a04a6a8131ef170
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-10 11:43:56 +0000

    refresh bag and checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,7 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_from': order_form,
+        'order_form': order_form,
     }
 
     return render(request, template, context)
