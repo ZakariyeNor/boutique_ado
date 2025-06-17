ce668619714fa10091169b8bc689f264e80bddff
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-17 09:12:02 +0000

    fixing f string error

--- bag/views.py
+++ bag/views.py
@@ -67,7 +67,7 @@ def adjust_bag(request, item_id):
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
             messages.success(
-                request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}'
+                request, f"Updated size {size.upper()} {product.name} quantity to {bag[item_id]['items_by_size'][size]}"
             )
         else:
             del bag[item_id]['items_by_size'][size]
