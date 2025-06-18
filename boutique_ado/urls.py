90882758d4bb72503d9ba13d2299e32b99170187
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-06-18 08:53:27 +0000

    add 404 handler

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,3 +1,7 @@
+from .views import handler404
+
+
+
 """boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -27,3 +31,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = "'boutique_ado.views.handler404"
